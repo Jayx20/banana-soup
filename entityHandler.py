72d795af850ee83a3fbffa26f34ac0e1fe2bd679
Jayx20 <jayx20@users.noreply.github.com>
2021-04-01 18:44:53 -0500

    made final changes for PR

--- entityHandler.py
+++ entityHandler.py
@@ -7,20 +7,18 @@ import random
 class EntityHandler:
     entity: Entity
     entityList: List[Entity] = []
+
+    # adds entity to handler and returns a reference to it
     def add(self, image: str, x: int, y: int):
-        newEntity = Entity(image, x, y)
-        self.entityList = []
-        self.entityList.append(newEntity)
-        print(newEntity.pos)
-    def position(self, entity: Entity, pos: Vector2):
-        for e in self.entityList:
-            if(e == entity):
-                e.pos = pos
-    def multiAdd(self, image: str, amount: int):
+        new_entity = Entity(image, x, y)
+        self.entityList.append(new_entity)
+        return new_entity
+
+    def multi_add(self, image: str, amount: int):
         self.entityList = []
         for x in range(amount):
-            newEntity = Entity(image, random.randint(40,1240), random.randint(40,680))
-            self.entityList.append(newEntity)
-            print(newEntity.pos)
+            new_entity = Entity(image, random.randint(40,1240), random.randint(40,680))
+            self.entityList.append(new_entity)
+            print(new_entity.pos)
 
 
--- main.py
+++ main.py
@@ -1,91 +1,88 @@
 import pygame
-from typing import List
 from entity import Entity
 from pygame import Vector2
-import entity
 import random
 from entityHandler import EntityHandler
+
+# Constants / Pygame stuff
 WIDTH, HEIGHT = 1280, 720
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 120
 pygame.display.set_caption("Banana Soup")
 
 # GLOBAL definitions
-FILL_COLOR = (200, 200, 255) #  Background color
-entities: List[Entity] = [] # List of active entities\
-blobNumber: int # Depricated
-test_entity: Entity # Test entity (The guy you use to walk around)
-sprint: int # Sprint
-prUpdate: bool # Print Rate Update //Depricated
-myfont: pygame.font # Fontz
-money: int = 0 # Money or blob value
-distanceX: int # Blob distance x
-distanceY: int # Blob distance y
-halfTick: int #Half tick of game clock (Runs something every half tick)
-eh: EntityHandler
+FILL_COLOR = (200, 200, 255)  # Background color
+myfont: pygame.font  # Fontz
+entities: EntityHandler  # List of active entities
+test_entity: Entity  # Test entity (The guy you use to walk around)
+sprint: int  # Sprint
+money: int = 0  # Money or blob value
+
+
 # runs once at the start of the game
 def init():
-    global test_entity, blob, money, myfont, blobNumber, halfTick, eh
-    eh = EntityHandler()
+    global test_entity, money, myfont, entities
+    entities = EntityHandler()
     money = 0
-    eh.multiAdd("blob.png", 10)
-    test_entity = Entity("goodsprite.png", 5, 5)
-    entities.append(test_entity)
+    entities.multi_add("blob.png", 10)
+    test_entity = entities.add("goodsprite.png", 5, 5)
     pygame.font.init()
     myfont = pygame.font.SysFont('Comic Sans MS', 30)
 
+
 # runs every frame - game logic
 def update():
-    global test_entity, money, blobNumber
-    
-    prUpdate = False
+    global test_entity, money, sprint
+
     # input stuff, move later to ma
     keys = pygame.key.get_pressed()
     sprint = 1
+    pr_update = False
     if keys[pygame.K_LSHIFT]:
         sprint += 1
     if keys[pygame.K_a]:
         test_entity.pos.x -= 1 * sprint
-        prUpdate = True
+        pr_update = True
     if keys[pygame.K_d]:
         test_entity.pos.x += 1 * sprint
-        prUpdate = True
+        pr_update = True
     if keys[pygame.K_w]:
         test_entity.pos.y -= 1 * sprint
-        prUpdate = True
+        pr_update = True
     if keys[pygame.K_s]:
         test_entity.pos.y += 1 * sprint
-        prUpdate = True
-    if prUpdate:
-         print("Sprite's x: " + str(test_entity.pos.x) + " Sprite's y: " + str(test_entity.pos.y), 1)
-         prUpdate = False
+        pr_update = True
+    if pr_update:
+        print("Sprite's x: " + str(test_entity.pos.x) + " Sprite's y: " + str(test_entity.pos.y), 1)
+
     # Pickup Blob
-    for entity in eh.entityList:
-        distanceX = test_entity.pos.x - entity.pos.x
-        distanceY = test_entity.pos.y - entity.pos.y
-        if abs(distanceX) < 20:
-            if abs(distanceY) < 20:
-                money += 1
-                eh.position(entity, Vector2(random.randint(40,1240), random.randint(40,680)))
+    for entity in entities.entityList:
+        # TODO: make better way to check if blob
+        if entity != test_entity:  # if blob
+            distance_x = test_entity.pos.x - entity.pos.x
+            distance_y = test_entity.pos.y - entity.pos.y
+            if abs(distance_x) < 20:
+                if abs(distance_y) < 20:
+                    money += 1
+                    entity.pos = Vector2(random.randint(40, 1240), random.randint(40, 680))
+
 
 # runs every frame - graphics
 def draw():
     WINDOW.fill(FILL_COLOR)
     # start drawing
-    for entity in entities:
+    for entity in entities.entityList:
         WINDOW.blit(entity.sprite.img, entity.pos, entity.sprite.rect)
-    for entity in eh.entityList:
-        WINDOW.blit(entity.sprite.img, entity.pos, entity.sprite.rect)
-    global myfont
-    textsurface = myfont.render("Blobs: " + str(money), False, (0, 0, 0))
-    WINDOW.blit(textsurface,(1080,0))
+
+    # draw money thing
+    text_surface = myfont.render("Blobs: " + str(money), False, (0, 0, 0))
+    WINDOW.blit(text_surface, (1080, 0))
 
     # finish drawing
     pygame.display.update()
 
 
 def main():
-    global halfTick
     init()
     clock = pygame.time.Clock()
     run = True
@@ -93,7 +90,7 @@ def main():
     while run:
         clock.tick(FPS)
         
-             # go through each event (user input)
+        # go through each event (user input)
         for event in pygame.event.get():
             # if user try to quit, close game
             if event.type == pygame.QUIT:
@@ -102,6 +99,8 @@ def main():
         draw()
 
     pygame.quit()
+
+
 # stupid python syntax for running main only from main.py
 if __name__ == "__main__":
     main()
